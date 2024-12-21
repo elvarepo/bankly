13a7c76c94d94d7047e09d04961b1287ba40cb52
Elva Wang <elvaworkingstation@gmail.com>
2024-12-20 18:11:23 -0800

    impletment inital file and folder structure with root and auth folder

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+export default async function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+  
+    return (
+      <main className="flex h-screen w-full font-inter">
+          {children}
+      </main>
+    );
+  }
